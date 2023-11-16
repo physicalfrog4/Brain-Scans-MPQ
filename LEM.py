255ad687b1665032d9f377bc431909476794fff3
Ali McNc <77911997+physicalfrog4@users.noreply.github.com>
2023-11-16 15:41:55 -0500

    new updates, cleaned up the code

--- LEM.py
+++ LEM.py
@@ -1,88 +1,14 @@
-import random
-from statistics import mean
 import torch
-import torch.nn as nn
-import torch.optim as optim
 from numpy import average
-from sklearn.linear_model import LinearRegression
-import torchvision.transforms as transforms
 import numpy as np
-from pathlib import Path
-from sklearn.linear_model import LinearRegression
 from sklearn.decomposition import IncrementalPCA
-from sklearn.metrics import balanced_accuracy_score, mean_absolute_error
-from sklearn.model_selection import KFold, cross_val_score, GridSearchCV
-from torch.utils.data import DataLoader, Dataset
-from torchvision import models
-from torchvision.models import GoogLeNet_Weights
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
 from torchvision.models.feature_extraction import get_graph_node_names, create_feature_extractor
 from tqdm import tqdm
-from PIL import Image
 from scipy.stats import pearsonr as corr
-from sklearn.linear_model import Ridge
-import visualize
 
-
-def splitData(args, modelGN, modelLR, train_img_list, test_img_list, train_img_dir, test_img_dir, lh_fmri, rh_fmri):
-    rand_seed = random.randint(0, 100)
-    np.random.seed(rand_seed)
-
-    global lh_correlation
-    global rh_correlation
-
-    # Calculate how many stimulus images correspond to 90% of the training data
-    num_train = int(np.round(len(train_img_list) / 100 * 90))
-    # Shuffle all training stimulus images
-    idxs = np.arange(len(train_img_list))
-    np.random.shuffle(idxs)
-    # Assign 90% of the shuffled stimulus images to the training partition, and 10% to the test partition
-    idxs_train, idxs_val = idxs[:num_train], idxs[num_train:]
-    # No need to shuffle or split the test stimulus images
-    idxs_test = np.arange(len(test_img_list))
-
-    print('Training stimulus images: ' + format(len(idxs_train)))
-    print('\nValidation stimulus images: ' + format(len(idxs_val)))
-    print('\nTest stimulus images: ' + format(len(idxs_test)))
-
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),  # resize the images to 224x24 pixels
-        transforms.ToTensor(),  # convert the images to a PyTorch tensor
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # normalize the images color channels
-    ])
-    batch_size = 100  # @param
-    # Get the paths of all image files
-    train_imgs_paths = sorted(list(Path(train_img_dir).iterdir()))
-    test_imgs_paths = sorted(list(Path(test_img_dir).iterdir()))
-
-    # The DataLoaders contain the ImageDataset class
-    train_imgs_dataloader = DataLoader(
-        ImageDataset(train_imgs_paths, idxs_train, transform),
-        batch_size=batch_size
-    )
-    val_imgs_dataloader = DataLoader(
-        ImageDataset(train_imgs_paths, idxs_val, transform),
-        batch_size=batch_size
-    )
-    test_imgs_dataloader = DataLoader(
-        ImageDataset(test_imgs_paths, idxs_test, transform),
-        batch_size=batch_size
-    )
-    lh_fmri_train = lh_fmri[idxs_train]
-    lh_fmri_val = lh_fmri[idxs_val]
-    rh_fmri_train = rh_fmri[idxs_train]
-    rh_fmri_val = rh_fmri[idxs_val]
-    del lh_fmri, rh_fmri
-    lh_fmri_train = normalize_fmri_data(lh_fmri_train)
-    rh_fmri_train = normalize_fmri_data(rh_fmri_train)
-    lh_fmri_val = normalize_fmri_data(lh_fmri_val)
-    rh_fmri_val = normalize_fmri_data(rh_fmri_val)
-
-    NNclassify(args, modelGN, modelLR, train_imgs_dataloader, val_imgs_dataloader, test_imgs_dataloader, batch_size,
-               lh_fmri_train, rh_fmri_train, lh_fmri_val, rh_fmri_val)
-
-
-def NNclassify(args, modelGN, modelLR, train_imgs_dataloader, val_imgs_dataloader, test_imgs_dataloader, batch_size,
-               lh_fmri_train, rh_fmri_train, lh_fmri_val, rh_fmri_val):
+def extract_data_features(modelGN, train_imgs_dataloader, val_imgs_dataloader, test_imgs_dataloader, batch_size):
     train_nodes, _ = get_graph_node_names(modelGN)
     model_layer = "classifier.0"
     feature_extractor = create_feature_extractor(modelGN, return_nodes=[model_layer])
@@ -104,32 +30,31 @@ def NNclassify(args, modelGN, modelLR, train_imgs_dataloader, val_imgs_dataloade
     print(features_test.shape)
     print('(Test stimulus images × PCA features)')
 
-    del modelGN, pca
-    linearMap(args, modelLR, features_train, lh_fmri_train, rh_fmri_train, features_val, features_test, lh_fmri_val,
-              rh_fmri_val)
-    print("Accuracy Scores")
+    del pca
+    return features_train, features_val, features_test
 
 
-def linearMap(args, modelLR, features_train, lh_fmri_train, rh_fmri_train, features_val, features_test, lh_fmri_val,
-              rh_fmri_val):
-    print("Start Linear Map")
-    # Fit a linear regression model on the training data
-    modelLR = LinearRegression().fit(features_train, lh_fmri_train)
+def linearMap(features_train, lh_fmri_train, rh_fmri_train, features_val, features_test, lh_fmri_val,rh_fmri_val):
+    modelLH = LinearRegression()
+    modelLH.fit(features_train, lh_fmri_train)
 
     # Use the fitted model to predict the validation and test fMRI data
-    lh_fmri_val_pred = modelLR.predict(features_val)
-    lh_fmri_test_pred = modelLR.predict(features_test)
+    lh_fmri_val_pred = modelLH.predict(features_val)
+    lh_fmri_test_pred = modelLH.predict(features_test)
 
     mae = mean_absolute_error(lh_fmri_val, lh_fmri_val_pred)
     print("Mean Absolute Error on Validation Data:", mae)
-
-    modelLR = LinearRegression().fit(features_train, rh_fmri_train)
+    modelRH = LinearRegression()
+    modelRH.fit(features_train, rh_fmri_train)
 
     # Use fitted linear regressions to predict the validation and test fMRI data
-    rh_fmri_val_pred = modelLR.predict(features_val)
-    rh_fmri_test_pred = modelLR.predict(features_test)
-    # predict the Accuracy
-    predAccuracy(args, lh_fmri_val_pred, lh_fmri_val, rh_fmri_val_pred, rh_fmri_val)
+    rh_fmri_val_pred = modelRH.predict(features_val)
+    rh_fmri_test_pred = modelRH.predict(features_test)
+
+    mae = mean_absolute_error(rh_fmri_val, rh_fmri_val_pred)
+    print("Mean Absolute Error on Validation Data:", mae)
+
+    return lh_fmri_val_pred, rh_fmri_val_pred, lh_fmri_test_pred, rh_fmri_test_pred
 
 
 def predAccuracy(args, lh_fmri_val_pred, lh_fmri_val, rh_fmri_val_pred, rh_fmri_val):
@@ -185,21 +110,3 @@ def fit_pca(feature_extractor, dataloader, batch_size):
         # Fit PCA to batch
         pca.partial_fit(ft.detach().cpu().numpy())
     return pca
-
-
-class ImageDataset(Dataset):
-    def __init__(self, imgs_paths, idxs, transform):
-        self.imgs_paths = np.array(imgs_paths)[idxs]
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.imgs_paths)
-
-    def __getitem__(self, idx):
-        # Load the image
-        img_path = self.imgs_paths[idx]
-        img = Image.open(img_path).convert('RGB')
-        # Preprocess the image and send it to the chosen device ('cpu' or 'cuda')
-        if self.transform:
-            img = self.transform(img).to('cuda')
-        return img
---
+++ classification.py
@@ -0,0 +1,117 @@
+import os
+import numpy as np
+import pandas as pd
+from PIL import Image
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, accuracy_score
+from sklearn.model_selection import train_test_split
+
+
+
+def classFMRIfromIMGandROI(args, train_img_dir, train_img_list, lh_fmri, rh_fmri, ImgClasses):
+    excelData = []
+    for img in range(len(ImgClasses)):
+        hemisphere = 'left'  # @param ['left', 'right'] {allow-input: true}
+        listroi = ["V1v", "V1d", "V2v", "V2d", "V3v", "V3d", "hV4", "EBA", "FBA-1", "FBA-2", "mTL-bodies", "OFA",
+                   "FFA-1", "FFA-2", "mTL-faces", "aTL-faces", "OPA", "PPA", "RSC", "OWFA", "VWFA-1", "VWFA-2",
+                   "mfs-words", "mTL-words", "early", "midventral", "midlateral", "midparietal", "ventral", "lateral",
+                   "parietal"]
+        for roi in listroi:
+            # Load the image
+            img_dir = os.path.join(train_img_dir, train_img_list[img])
+            train_img = Image.open(img_dir).convert('RGB')
+
+            if roi in ["V1v", "V1d", "V2v", "V2d", "V3v", "V3d", "hV4"]:
+                roi_class = 'prf-visualrois'
+            elif roi in ["EBA", "FBA-1", "FBA-2", "mTL-bodies"]:
+                roi_class = 'floc-bodies'
+            elif roi in ["OFA", "FFA-1", "FFA-2", "mTL-faces", "aTL-faces"]:
+                roi_class = 'floc-faces'
+            elif roi in ["OPA", "PPA", "RSC"]:
+                roi_class = 'floc-places'
+            elif roi in ["OWFA", "VWFA-1", "VWFA-2", "mfs-words", "mTL-words"]:
+                roi_class = 'floc-words'
+            elif roi in ["early", "midventral", "midlateral", "midparietal", "ventral", "lateral", "parietal"]:
+                roi_class = 'streams'
+
+            # Load the ROI brain surface maps
+            challenge_roi_class_dir = os.path.join(args.data_dir, 'roi_masks',
+                                                   hemisphere[0] + 'h.' + roi_class + '_challenge_space.npy')
+            fsaverage_roi_class_dir = os.path.join(args.data_dir, 'roi_masks',
+                                                   hemisphere[0] + 'h.' + roi_class + '_fsaverage_space.npy')
+            roi_map_dir = os.path.join(args.data_dir, 'roi_masks',
+                                       'mapping_' + roi_class + '.npy')
+            challenge_roi_class = np.load(challenge_roi_class_dir)
+            fsaverage_roi_class = np.load(fsaverage_roi_class_dir)
+            roi_map = np.load(roi_map_dir, allow_pickle=True).item()
+
+            # Select the vertices corresponding to the ROI of interest
+            roi_mapping = list(roi_map.keys())[list(roi_map.values()).index(roi)]
+            challenge_roi = np.asarray(challenge_roi_class == roi_mapping, dtype=int)
+            fsaverage_roi = np.asarray(fsaverage_roi_class == roi_mapping, dtype=int)
+
+            # Map the fMRI data onto the brain surface map
+            fsaverage_response = np.zeros(len(fsaverage_roi))
+            if hemisphere == 'left':
+                fsaverage_response[np.where(fsaverage_roi)[0]] = \
+                    lh_fmri[img, np.where(challenge_roi)[0]]
+            elif hemisphere == 'right':
+                fsaverage_response[np.where(fsaverage_roi)[0]] = \
+                    rh_fmri[img, np.where(challenge_roi)[0]]
+            accuracy = np.mean(fsaverage_response[np.where(fsaverage_roi)[0]])
+            if accuracy > 0.001:
+                excelData.append([ImgClasses[img], roi, accuracy])
+
+    # print(excelData)
+    df = pd.DataFrame(excelData, columns=['Name', 'ROI', 'Accuracy'])
+    #df.to_excel('class_data.xlsx', index=False)
+
+    # print(df['ROI'].values, df['Accuracy'])
+    # plt.hist(excelData['Accuracy'], color='lightgreen', ec='black', bins=15)
+    # plt.show()
+    # feature encoding
+
+    # Encode categorical variables like 'Name' and 'ROI' using one-hot encoding
+    df_encoded = pd.get_dummies(df, columns=['Name', 'ROI'], drop_first=True)
+
+    # Split the data into features (X) and target variable (y)
+    X = df_encoded.drop('Accuracy', axis=1)
+    y = df_encoded['Accuracy']
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Create and fit the linear regression model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    # Make predictions on the test set
+    y_pred = model.predict(X_test)
+
+    # Evaluate the model performance
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    mae = mean_absolute_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    adj_r2 = 1 - (1 - r2) * (len(y_test) - 1) / (len(y_test) - X_test.shape[1] - 1)
+
+    print(f"Root Mean Squared Error: {rmse}")
+    print(f"Mean Absolute Error: {mae}")
+    print(f"R-squared: {r2}")
+    print(f"Adjusted R-squared: {adj_r2}")
+    accuracy_score = model.score(X_test, y_test)
+    print("accuracy score", accuracy_score)
+
+    normAcc = normalize_fmri_data2(df['Accuracy'])
+    print(normAcc)
+    df['Accuracy'] = normAcc
+    print(df['Accuracy'])
+    print(df)
+    return df
+
+
+def normalize_fmri_data2(data):
+    mean_value = np.mean(data)
+    std_dev = np.std(data)
+    normalized_data = (data - mean_value) / std_dev
+    return normalized_data
