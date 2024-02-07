c16d96fcce635202e2dc75b9025167597dd5b13c
Jose Morales <jamoralesl2021@gmail.com>
2024-02-07 14:38:24 -0500

    Added code to train base VGG model on coco label

---
+++ gradCam/datasets.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+import torch
+from torchvision import transforms
+from torch.utils.data import Dataset
+from PIL import Image
+
+def getFileNames(parentDir: str, subj: int):
+    imgsPath = f"subj0{subj}/training_split/training_images/"
+    filesDir = os.path.join(parentDir,imgsPath)
+    fileNames = os.listdir(filesDir)
+    imgPaths = [os.path.join(parentDir, imgsPath, img) for img in fileNames]
+    return imgPaths
+
+
+def getImgLabels(metaDataDir: str, subj: int):
+    filePath = os.path.join(metaDataDir, F"subj0{subj}ImgData.pkl")
+    pklData = pd.read_pickle(filePath)
+    return pd.get_dummies(pklData["classLabel"], dtype=float).to_numpy()
+
+
+
+class COCOImgWithLabel(Dataset):
+    def __init__(self, parentDir: str, metaDataDir: str, subj: int, tsfms = None):
+        self.tsfms = tsfms        
+        self.imgPaths = getFileNames(parentDir, subj)
+        self.labels = getImgLabels(metaDataDir, subj)
+    def __len__(self):
+        return len(self.imgPaths)
+    def __getitem__(self, idx):
+        img = Image.open(self.imgPaths[idx])
+        label = self.labels[idx]
+        if self.tsfms:
+            img = self.tsfms(img)
+        return img, torch.from_numpy(label)
+    
